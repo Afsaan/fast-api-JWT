eb71cba548980e8e4adc4a825694a2564114b0e9
Afsan Khan <afsankhan@Afsans-MacBook-Air.local>
2023-03-04 00:22:51 +0530

    added jwt handler

---
+++ app/auth/jwt_handler.py
@@ -0,0 +1,33 @@
+"""
+this file will be responsible for signing, encoding and decoding and return JWTs"
+"""
+
+import time
+import jwt
+from decouple import config
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+#functions returns the generated tokens
+def token_response(token: str):
+    return {
+        "access_token": token,
+    }
+
+def signJWT(UserID : str):
+    payload = {
+        "user_id": UserID,
+        "expiry": time.time() + 600
+    }
+
+    token = jwt.encode(payload , JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token_response(token)
+
+def decodeJWT(token : str):
+    try:
+        decode_token = jwt.decode(token, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+        return decode_token if decode_token['expiry'] >= time.time() else None
+    except:
+        return {}
--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-from tkinter import Y
-from fastapi import Fastapi
+from fastapi import FastAPI
+from app.models import PostSchema
+
+posts = [
+    {
+        "id" : 1,
+        "title" : "panda",
+        "content" : "lazy animals"
+    },
+
+    {
+        "id" : 2,
+        "title" : "elephant",
+        "content" : "big animals"
+    },
+
+]
+
+app = FastAPI()
 
 
-app = Fastapi()
+#basic home page
+@app.get("/", tags = ["Greet"])
+def get_post():
+    return {"Hello": "Welcome to post page"}
 
+
+# get
+@app.get("/posts", tags = ["posts"])
+def get_post():
+    return {"data": posts}
+
+@app.get("/post/{id}" , tags = ["posts"])
+def get_one_post(id : int):
+    if id > len(posts):
+        return {"error": "post with the id does not exist"}
+
+    for post in posts:
+        if post["id"] == id:
+            return {"data": post}
+
+
+@app.post("/posts", tags = ["posts"])
+def add_post(post : PostSchema):
+    post.id = len(posts) + 1
+    posts.apend(post.dict())
+
+    return {"data": "Post Added SuccessFully"}
